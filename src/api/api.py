d810b7d0147ce58fdf321fd4527fb063421cf008
ArberBajraktari <arberbajraktari02@gmail.com>
2023-04-22 16:44:09 +0200

    formated

--- src/api/api.py
+++ src/api/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends
 from .endpoints import dashboard, users
-
+from ..users.manager import current_active_user
 
 router = APIRouter()
 
-router.include_router(dashboard.router)
+router.include_router(
+    dashboard.router, dependencies=[Depends(current_active_user)], tags=["dashboard"]
+)
 router.include_router(users.router)
