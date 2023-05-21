9094ac8c4d2a36491bb6709d0997c681dc2a2cb7
ArberBajraktari <arberbajraktari02@gmail.com>
2023-05-21 13:22:12 +0200

    Taged projects url

--- src/api/api.py
+++ src/api/api.py
@@ -11,4 +11,4 @@ router.include_router(
 router.include_router(users.router)
 router.include_router(tasks.router, tags=["tasks"])
 router.include_router(tasksItem.router, tags=["tasks_item"])
-router.include_router(projects.router, tags=["tasks_item"])
+router.include_router(projects.router, tags=["projects"])
